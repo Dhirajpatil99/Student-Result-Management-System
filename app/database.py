12b84a1d927d1b238f6350fd16c128ab0d21372d
Dhiraj patil <114562965+Dhirajpatil99@users.noreply.github.com>
2023-09-19 11:15:03 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import table_functions as tf
 
 
 
-db=tf.create_table("localhost","root","root","management_system")
+db=tf.create_table("xxx","xxx","xxx","xxxx")
 #creating teacher table if not created
 db.create_teacher_table()
 #creating course table if not created
